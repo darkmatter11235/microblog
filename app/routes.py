a1f1b4d4ffa4b8aa452c44a6f3ef24e589e8e63f
darkmatter11235 <darkmatter11235@github.com>
2018-05-17 21:31:56 -0700

    adding templates

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'DarkMatter'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'cartman'},
+            'body': 'Suck my balls Kyle!'
+        },
+        {
+            'author': {'username': 'kyle' },
+            'body': 'Shutup cartman!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
